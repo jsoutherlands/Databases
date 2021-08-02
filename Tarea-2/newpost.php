4309088dfe2ce7dfaffd1c8d07169d9c46919de1
José Southerland Silva <82189997+jsoutherlands@users.noreply.github.com>
2021-08-02 03:23:23 -0400

    Update newpost.php

--- Tarea-2/newpost.php
+++ Tarea-2/newpost.php
@@ -4,12 +4,17 @@
 		<form action="includes/newpost.inc.php" method="post">
 			<textarea name="usmito" placeholder="¿En qué estás pensando?"></textarea>
 			<div>
-				<input type="text" name="tags" placeholder="Ingresa tags relacionados. ¡Recuerda separar por comas!">
+				<span>Ingresa tus tags a continuación:</span>
+			</div>
+			<div class="tag-container" data-name="tag-container">
+			</div>
+			<script src="js/tags.js"></script>
+			<div>
 				<select name="privacidad">
 					<option value="Público">Público</option>
 					<option value="Amigos Cercanos">Amigos Cercanos</option>
 				</select>
-				<button type="submit" name="submit">Publicar</button>
+				<button type="submit" name="submit" >Publicar</button>
 			</div>
 		</form>
 	</section>
